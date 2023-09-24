7b6271ccd7353014bb3525fa8402009832a39a8d
Kevin Muscara <muscarak1@nku.edu>
2023-09-24 15:15:11 -0400

    basic starter

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import FAQ from "./components/FAQ";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Past from "./components/Past";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Past />
+      <FAQ />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
